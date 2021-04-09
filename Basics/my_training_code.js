a9c6134287e8a7a5f33c84880c4d0159ebdeda62
=KayseMca <=madadmca04@gmail.com>
2021-04-09 00:34:02 -0700

    changed name

---
+++ Basics/my_training_code.js
@@ -0,0 +1,22 @@
+console.log("Hello World!")
+
+console.log("Started today learning Javascript!")
+
+//Variables
+
+// Variables is like container that we can store a valuable to use couple of times
+
+var javascript = "Javascript"
+var myName = "Kayse"
+var age = 15
+
+console.log(javascript + myName + age) // concating
+
+
+//Boolean
+var exist = true;
+console.log(exist)
+
+alert("you just called me to pop up the head of page")
+// getting from user data
+var university = prompt("put your university here?")
